03756a2e29878450ae000a6596bfeff4a0987544
karthik12karthik12 <114928376+karthik12karthik12@users.noreply.github.com>
2024-01-21 13:58:58 +0530

    Hi
    Jui

---
+++ sample class .py
@@ -0,0 +1,10 @@
+class student:
+	mark1,mark2,mark3=71,75,85
+	def process(self):
+		sum=student.mark1,student.mark2,student.mark3
+		avg=sum/3
+		print("Total Marks=",sum)
+		print("Average Marks",avg)
+		return
+s=student()
+s.process()		
